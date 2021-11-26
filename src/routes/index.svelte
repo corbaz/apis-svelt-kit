60c6a578311b6d9ca0d0809acb8b3a8b27f8537d
Julio Corbaz <julio.corbaz@gmail.com>
2021-11-26 10:18:20 -0300

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<main>
+	<h1 class="text-2xl font-bold text-[#00f]">I have a custom color!</h1>
+</main>
+<h1>Bienvenidos a SvelteKit</h1>
+<p>Visite
+	<a class="text-blue-500" href="https://sveltekit.com">SvelteKit</a>
+	para leer la documentación
+</p>
---
+++ svelte.config.js
@@ -0,0 +1,71 @@
+// import adapter from '@sveltejs/adapter-auto';
+//
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter(),
+//
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		target: '#svelte'
+// 	}
+// };
+
+// export default config;
+
+
+
+// import node from '@sveltejs/adapter-node';
+// const pkg = require('./package.json');
+//
+// /** @type {import('@sveltejs/kit').Config} */
+// module.exports = {
+// 	kit: {
+// 		// By default, `npm run build` will create a standard Node app.
+// 		// You can create optimized builds for different platforms by
+// 		// specifying a different adapter
+// 		adapter: node(),
+//
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		target: '#svelte',
+//
+// 		vite: {
+// 			ssr: {
+// 				noExternal: Object.keys(pkg.dependencies || {})
+// 			}
+// 		}
+// 	}
+// };
+
+import sveltePreprocess from 'svelte-preprocess'
+import node from '@sveltejs/adapter-node'
+/*
+const sveltePreprocess = require("svelte-preprocess");
+const node = require('@sveltejs/adapter-node');
+const pkg = require('./package.json');
+*/
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+	preprocess: [
+		sveltePreprocess({
+			defaults: {
+				style: "postcss",
+			},
+			postcss: true
+		}),
+	],
+	kit: {
+		// By default, `npm run build` will create a standard Node app.
+		// You can create optimized builds for different platforms by
+		// specifying a different adapter
+		adapter: node(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		vite: {
+			ssr: {
+				noExternal: '' // (pkg.dependencies || {})
+			}
+		}
+	}
+};
