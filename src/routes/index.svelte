5c51117a1101695ddfd60f03f8e69121dd570fda
Julio Corbaz <julio.corbaz@gmail.com>
2021-11-27 17:04:43 -0300

    'update'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,82 +9,47 @@
 	};
 </script>
 
-<!--<script>-->
-<!--	let buscador;-->
-<!--	const buscar = async () => {-->
-<!--		const respuesta = await fetch(`/api/buscar/${buscador.value}`);-->
-<!--		const peliculas = await respuesta.json();-->
-<!--		console.log(peliculas);-->
-<!--	};-->
-<!--</script>-->
-
-<!--<section>-->
-<!--	<div>-->
-<!--		<h1 class='text-3xl bg-indigo-700 text-yellow-400 text-center	p-5'>-->
-<!--			Buscador de Gif´s y Sticker´s-->
-<!--		</h1>-->
-<!--		<div class='py-8 mx-2 grid grid-cols-10 gap-10'>-->
-<!--			<div class='text-gray-700 text-sm font-bold-->
-<!--			 				col-span-2 text-center py-5'>-->
-<!--				Gif´s o Sticker´s-->
-<!--			</div>-->
-<!--			<input class='shadow appearance-none border rounded w-full py-2 px-3-->
-<!--						 text-gray-700 leading-tight focus:outline-none-->
-<!--						 focus:shadow-outline col-span-6'-->
-<!--						 id='inputBusqueda' type='text'-->
-<!--						 bind:value={buscador}-->
-<!--						 placeholder='Ingrese el Gif o Sticker que desea buscar'>-->
-
-<!--			<button on:click={buscar}-->
-<!--							class='bg-blue-500 hover:bg-blue-700 text-white font-bold-->
-<!--							py-2 px-4 rounded col-span-2'>-->
-<!--				Buscar-->
-<!--			</button>-->
-<!--		</div>-->
-<!--	</div>-->
-
 <section class="">
-	<div class="flex flex-col items-center justify-center">
+	<div class="flex flex-col justify-center items-center">
 		<div
-			class="text-3xl
-		 						md:text-5xl
-		 						text-center text-white font-semibold mb-5"
+			class="text-center text-white font-semibold mb-5
+		 					text-3xl	md:text-5xl"
 		>
 			{artista_buscado.length > 0 ? artista_buscado : 'Buscar un Artista'}
-			<div
-				class="w-full flex flex-row justify-between
-		 rounded-md bg-white bg-opacity-20 p-1 gap-1
-								border border-white border-opacity-30"
-			>
-				<input
-					class="w-full px-2 bg-transparent text-white"
-					type="text"
-					bind:value={artista_buscado}
-					on:keydown={(evento) => {
-						if (evento.keyCode !== 13) return;
-						search();
-					}}
-				/>
-				<button on:click={search}>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="text-red-400 h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-						/>
-					</svg>
-				</button>
-			</div>
-			<div class="w-full pt-10 flex justify-center">
-				<img src={logo} alt="logo" class="h-60 w-60" />
-			</div>
+		</div>
+		<div
+			class="flex flex-row p-1 w-full bg-white bg-opacity-20
+			 rounded-md border-white border-opacity-30 justify-between
+			 	 gap-1 border"
+		>
+			<input
+				bind:value={artista_buscado}
+				class="px-2 w-full text-white bg-transparent"
+				on:keydown={(evento) => {
+					if (evento.keyCode !== 13) return;
+					search();
+				}}
+				type="text"
+			/>
+			<button on:click={search}>
+				<svg
+					class="w-6 h-6 text-red-400"
+					fill="none"
+					stroke="currentColor"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+					/>
+				</svg>
+			</button>
+		</div>
+		<div class="flex justify-center pt-10 w-full">
+			<img alt="logo" class="w-60 h-60" src={logo} />
 		</div>
 	</div>
 </section>
