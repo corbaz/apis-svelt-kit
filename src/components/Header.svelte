b859ebc04223c2c9781ec2d36a632a576fed773a
Julio Corbaz <julio.corbaz@gmail.com>
2021-11-28 19:26:58 -0300

    'update'

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,22 +9,28 @@
 	};
 </script>
 
-<div class='fixed z-50 bg-black w-full flex flex-row justify-end py-3 px-10 md:px-20 lg:px-52'>
-	<button class='flex items-center justify-center text-white ml-2'
+<div class='fixed z-50 bg-black grid grid-cols-3 w-full py-3 px-10 md:px-20 lg:px-52'>
+	<button class='col-span-1 text-white ml-2'
 					on:click={goBack}>
 		<img alt='back' class='h-4' src={back} />
 	</button>
-
-	<button
-		class='flex items-center justify-center text-white ml-2'
-		on:click={() => {
-			goto('/');
-		}}
-	>
-		<img alt='back' class='h-6' src={home} />
-	</button>
-	<a class='ml-2' href='https://www.linkedin.com/in/julio-corbaz/'>
-		<img alt='logo' class='h-6' src={logo} target='_blank' />
-	</a>
-	<span class='text-white ml-2'>Api Music</span>
+	<div class='col-span-1 mx-auto'>
+		<span class='text-white'>Api Music</span>
+	</div>
+	<div class='col-span-1'>
+		<div class='grid grid-cols-3 gap-2'>
+			<div class='col-span-1'></div>
+			<button class='text-white col-span-1 ml-auto'
+							on:click={() => {
+								goto('/');
+							}}
+			>
+				<img alt='back' class='h-6' src={home} />
+			</button>
+			<a class='col-span-1 ml-auto'
+				 href='https://www.linkedin.com/in/julio-corbaz/'>
+				<img alt='logo' class='h-6' src={logo} target='_blank' />
+			</a>
+		</div>
+	</div>
 </div>
